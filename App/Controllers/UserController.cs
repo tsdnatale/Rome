65a239ccf3bf872198e1419b6d7ff1d846a8106a
Tim Natale <tsdnatale@gmail.com>
2021-03-08 07:19:36 -0800

    setup user controller

--- App/Controllers/UserController.cs
+++ App/Controllers/UserController.cs
@@ -1,4 +1,12 @@
+using Microsoft.Extensions.Logging;
+
 namespace Romeo.App.Controllers
 {
+    public class UserController : BaseApiController<UserController>
+    {
+        public UserController(ILogger<UserController> logger) : base(logger)
+        {
 
+        }
+    }
 }
